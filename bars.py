6536534c92f8806bfa3337ab2e48f9cbd0c92269
Alexander <Alexander>
2016-12-01 12:08:03 +0300

    data variable rename

--- bars.py
+++ bars.py
@@ -6,24 +6,24 @@ def load_data(filepath):
         return json.load(data_file)
 
 
-def get_biggest_bar(data):
-    maximum_seats = max(data, key=lambda x: x['Cells']['SeatsCount'])
+def get_biggest_bar(bars):
+    maximum_seats = max(bars, key=lambda x: x['Cells']['SeatsCount'])
     return maximum_seats['Cells']['Name']
 
 
-def get_smallest_bar(data):
-    minimun_seats = min(data, key=lambda x: x['Cells']['SeatsCount'])
+def get_smallest_bar(bars):
+    minimun_seats = min(bars, key=lambda x: x['Cells']['SeatsCount'])
     return minimun_seats['Cells']['Name']
 
 
-def get_closest_bar(data, longitude, latitude):
-    closest_bar = min(data, key=lambda x: (x['Cells']['geoData']['coordinates'][0]-longitude) ** 2 + (x['Cells']['geoData']['coordinates'][1]-latitude) ** 2)
+def get_closest_bar(bars, longitude, latitude):
+    closest_bar = min(bars, key=lambda x: (x['Cells']['geoData']['coordinates'][0]-longitude) ** 2 + (x['Cells']['geoData']['coordinates'][1]-latitude) ** 2)
     return closest_bar['Cells']['Name']
 
 if __name__ == '__main__':
-    data = load_data(input('Введите путь до файла: '))
-    print('Самый большой бар: {}' .format(get_biggest_bar(data)))
-    print('Самый маленький бар: {}' .format(get_smallest_bar(data)))
+    all_bars_information_json = load_data(input('Введите путь до файла: '))
+    print('Самый большой бар: {}' .format(get_biggest_bar(all_bars_information_json)))
+    print('Самый маленький бар: {}' .format(get_smallest_bar(all_bars_information_json)))
     longitude = float(input('Введите долготу: '))
     latitude = float(input('Введите широту: '))
-    print('Самый близкий бар: {}' .format(get_closest_bar(data, longitude, latitude)))
+    print('Самый близкий бар: {}' .format(get_closest_bar(all_bars_information_json, longitude, latitude)))
