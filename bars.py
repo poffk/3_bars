a1d4b0b4560553adc3f7a3a3b6e7fb4e0ebc5212
Alexander <Alexander>
2016-12-01 11:04:02 +0300

    function

--- bars.py
+++ bars.py
@@ -17,20 +17,11 @@ def get_smallest_bar(data):
 
 
 def get_closest_bar(data, longitude, latitude):
-    coord = data[0]['Cells']['geoData']['coordinates']
-    minimum_range = (coord[0]-longitude) ** 2 + (coord[1]-latitude) ** 2
-    number_bars = len(data)
-    for number_of_bar in range(number_bars):
-        coord = data[number_of_bar]['Cells']['geoData']['coordinates']
-        distance = (coord[0]-longitude) ** 2 + (coord[1]-latitude) ** 2
-        if distance < minimum_range:
-            minimum_range = distance
-            closest_bar_name = data[number_of_bar]['Cells']['Name']
-    return closest_bar_name
-
+    closest_bar = min(data, key=lambda x: (x['Cells']['geoData']['coordinates'][0]-longitude) ** 2 + (x['Cells']['geoData']['coordinates'][1]-latitude) ** 2)
+    return closest_bar['Cells']['Name']
 
 if __name__ == '__main__':
-    data = (load_data(input('Введите путь до файла: ')))
+    data = load_data(input('Введите путь до файла: '))
     print('Самый большой бар: {}' .format(get_biggest_bar(data)))
     print('Самый маленький бар: {}' .format(get_smallest_bar(data)))
     longitude = float(input('Введите долготу: '))
